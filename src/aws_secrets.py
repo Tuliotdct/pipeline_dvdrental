8c08d0e846185b068f9f126179307fbafb916572
Tulio Tome <tuliotdct@gmail.com>
2025-11-15 17:26:24 +0100

    chore: add aws_secrets to retrieve postgres rds secrets

---
+++ src/aws_secrets.py
@@ -0,0 +1,29 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+import os
+from dotenv import load_dotenv
+
+def get_secret():
+
+    load_dotenv()
+
+    secret_name = os.getenv("SECRET_NAME")
+    region_name = os.getenv("REGION_NAME")
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = json.loads(get_secret_value_response['SecretString'])
+
+    return secret
